d31147ebb85762607aa3d29d615062ddf2a6f275
hmm007 <hmmpython@gmail.com>
2017-02-16 09:35:57 +0100

    Create app.php

---
+++ app/resources/views/app.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+	  <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Example</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+      </head>
+  <body>
+  	<div id="root"></div>
+    <div id="app"></div>
+    <script type="text/javascript" src="bundle.js"></script>
+  </body>
+</html>
